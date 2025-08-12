40b8aa32997b657def68972e56523668e0241f68
Peter <85247098+RedFox86@users.noreply.github.com>
2025-08-12 09:56:48 -0400

    Add aluminum brass

---
+++ src/main/java/net/redfox/metalica/material/TinkersMaterialBuilder.java
@@ -0,0 +1,136 @@
+package net.redfox.metalica.material;
+
+import net.minecraft.world.item.Tier;
+import slimeknights.tconstruct.library.materials.stats.IMaterialStats;
+import slimeknights.tconstruct.tools.stats.*;
+
+import java.util.ArrayList;
+
+public class TinkersMaterialBuilder {
+  private final ArrayList<IMaterialStats> statList = new ArrayList<>();
+
+  private HeadMaterialStats headStats;
+  private HandleMaterialStats handleStats;
+  private PlatingMaterialStats helmetStats;
+  private PlatingMaterialStats chestplateStats;
+  private PlatingMaterialStats leggingsStats;
+  private PlatingMaterialStats bootsStats;
+  private PlatingMaterialStats shieldStats;
+  private GripMaterialStats gripStats;
+  private LimbMaterialStats limbStats;
+  private String modifier;
+  private final Tier tier;
+  private final int temperature;
+
+  public TinkersMaterialBuilder(Tier tier, int temperature) {
+    this.tier = tier;
+    this.temperature = temperature;
+  }
+
+  public TinkersMaterialBuilder modifier(String modifier) {
+    this.modifier = modifier;
+    return this;
+  }
+
+  public TinkersMaterialBuilder head(int durability, float miningSpeed, float attackDamage) {
+    this.headStats = new HeadMaterialStats(durability, miningSpeed, tier, attackDamage);
+    statList.add(headStats);
+    return this;
+  }
+
+  public TinkersMaterialBuilder handle(int durability, float miningSpeed, float meleeSpeed, float attackDamage) {
+    this.handleStats = new HandleMaterialStats(durability, miningSpeed, meleeSpeed, attackDamage);
+    statList.add(handleStats);
+    return this;
+  }
+
+  public TinkersMaterialBuilder helmet(int durability, float armor, float armorToughness, float knockbackResistance) {
+    this.helmetStats = new PlatingMaterialStats(PlatingMaterialStats.HELMET, durability, armor, armorToughness, knockbackResistance);
+    statList.add(helmetStats);
+    return this;
+  }
+  public TinkersMaterialBuilder chestplate(int durability, float armor, float armorToughness, float knockbackResistance) {
+    this.chestplateStats = new PlatingMaterialStats(PlatingMaterialStats.CHESTPLATE, durability, armor, armorToughness, knockbackResistance);
+    statList.add(chestplateStats);
+    return this;
+  }
+  public TinkersMaterialBuilder leggings(int durability, float armor, float armorToughness, float knockbackResistance) {
+    this.leggingsStats = new PlatingMaterialStats(PlatingMaterialStats.LEGGINGS, durability, armor, armorToughness, knockbackResistance);
+    statList.add(leggingsStats);
+    return this;
+  }
+  public TinkersMaterialBuilder boots(int durability, float armor, float armorToughness, float knockbackResistance) {
+    this.bootsStats = new PlatingMaterialStats(PlatingMaterialStats.BOOTS, durability, armor, armorToughness, knockbackResistance);
+    statList.add(bootsStats);
+    return this;
+  }
+  public TinkersMaterialBuilder shield(int durability, float armor, float armorToughness, float knockbackResistance) {
+    this.shieldStats = new PlatingMaterialStats(PlatingMaterialStats.SHIELD, durability, armor, armorToughness, knockbackResistance);
+    statList.add(shieldStats);
+    return this;
+  }
+
+  public TinkersMaterialBuilder grip(int durability, float accuracy, float meleeDamage) {
+    this.gripStats = new GripMaterialStats(durability, accuracy, meleeDamage);
+    statList.add(gripStats);
+    return this;
+  }
+
+  public TinkersMaterialBuilder limb(int durability, float drawSpeed, float velocity, float accuracy) {
+    this.limbStats = new LimbMaterialStats(durability, drawSpeed, velocity, accuracy);
+    statList.add(limbStats);
+    return this;
+  }
+
+  public HeadMaterialStats getHeadStats() {
+    return headStats;
+  }
+
+  public HandleMaterialStats getHandleStats() {
+    return handleStats;
+  }
+
+  public PlatingMaterialStats getHelmetStats() {
+    return helmetStats;
+  }
+
+  public PlatingMaterialStats getChestplateStats() {
+    return chestplateStats;
+  }
+
+  public PlatingMaterialStats getLeggingsStats() {
+    return leggingsStats;
+  }
+
+  public PlatingMaterialStats getBootsStats() {
+    return bootsStats;
+  }
+
+  public PlatingMaterialStats getShieldStats() {
+    return shieldStats;
+  }
+
+  public GripMaterialStats getGripStats() {
+    return gripStats;
+  }
+
+  public LimbMaterialStats getLimbStats() {
+    return limbStats;
+  }
+
+  public Tier getTier() {
+    return tier;
+  }
+
+  public int getTemperature() {
+    return temperature;
+  }
+
+  public String getModifier() {
+    return modifier;
+  }
+
+  public ArrayList<IMaterialStats> getStatList() {
+    return statList;
+  }
+}
