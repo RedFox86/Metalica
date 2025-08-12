10c978095ebeaca4b6d954b0f1141e24f4c7c248
Peter <85247098+RedFox86@users.noreply.github.com>
2025-08-12 10:03:18 -0400

    Change lang to skip raw translations
    if a material doesn't have ores / raw ores, don't generate a translation for it.

--- src/main/java/net/redfox/metalica/datagen/ModLanguageProvider.java
+++ src/main/java/net/redfox/metalica/datagen/ModLanguageProvider.java
@@ -17,16 +17,18 @@ public class ModLanguageProvider extends LanguageProvider {
     add("creativetab."+Metalica.MOD_ID+".metalica_tab", "Metalica");
 
     for (MetalMaterial material : MetalMaterial.getMaterials()) {
-      add("block."+Metalica.MOD_ID+"."+material.getName()+"_ore", material.getDisplayName()+" Ore");
-      add("block."+Metalica.MOD_ID+"."+"deepslate_"+material.getName()+"_ore", "Deepslate "+material.getDisplayName()+" Ore");
       add("block."+Metalica.MOD_ID+"."+material.getName()+"_block", material.getDisplayName()+" Block");
-      add("block."+Metalica.MOD_ID+"."+"raw_"+material.getName()+"_block", "Raw "+material.getDisplayName()+" Block");
       add("item."+Metalica.MOD_ID+"."+material.getName()+"_ingot", material.getDisplayName()+" Ingot");
       add("item."+Metalica.MOD_ID+"."+material.getName()+"_dust", material.getDisplayName()+" Dust");
       add("item."+Metalica.MOD_ID+"."+material.getName()+"_nugget", material.getDisplayName()+" Nugget");
-      add("item."+Metalica.MOD_ID+"."+"raw_"+material.getName(), "Raw "+material.getDisplayName());
       add("item."+Metalica.MOD_ID+"."+"molten_"+material.getName()+"_bucket", "Molten "+material.getDisplayName()+" Bucket");
       add("fluid."+Metalica.MOD_ID+"."+"molten_"+material.getName(), "Molten "+material.getDisplayName());
+      if (material.hasOre()) {
+        add("block."+Metalica.MOD_ID+"."+"deepslate_"+material.getName()+"_ore", "Deepslate "+material.getDisplayName()+" Ore");
+        add("block."+Metalica.MOD_ID+"."+material.getName()+"_ore", material.getDisplayName()+" Ore");
+        add("item."+Metalica.MOD_ID+"."+"raw_"+material.getName(), "Raw "+material.getDisplayName());
+        add("block."+Metalica.MOD_ID+"."+"raw_"+material.getName()+"_block", "Raw "+material.getDisplayName()+" Block");
+      }
     }
   }
 }
