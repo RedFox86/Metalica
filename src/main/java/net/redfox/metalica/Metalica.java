a7377f2517cc55069d75c6f1b725b3da83d3f7df
Peter <85247098+RedFox86@users.noreply.github.com>
2025-08-14 18:23:19 -0400

    Add conditional mantle loading

--- src/main/java/net/redfox/metalica/Metalica.java
+++ src/main/java/net/redfox/metalica/Metalica.java
@@ -5,6 +5,7 @@ import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
@@ -25,8 +26,6 @@ public class Metalica {
     public static final Logger LOGGER = LogUtils.getLogger();
 
     public Metalica(FMLJavaModLoadingContext context) {
-
-
         Metals.createMaterials();
 
         IEventBus modEventBus = context.getModEventBus();
@@ -35,10 +34,9 @@ public class Metalica {
 
         ModItems.register(modEventBus);
         ModBlocks.register(modEventBus);
-        ModFluids.register(modEventBus);
-
-        // Register the commonSetup method for modloading
-        modEventBus.addListener(this::commonSetup);
+        if (ModList.get().isLoaded("mantle")) {
+            ModFluids.register(modEventBus);
+        }
 
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
